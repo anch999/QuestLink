05a82aba85f99ad10635c046b79aa33177abf4f9
Anch <adrianpratt.ap@gmail.com>
2024-04-01 04:47:53 +1300

    swapped to using the ascension button

--- QuestLink/QuestLink.lua
+++ QuestLink/QuestLink.lua
@@ -1,65 +1,66 @@
 QuestLink = LibStub("AceAddon-3.0"):NewAddon("QuestLink", "AceConsole-3.0", "AceEvent-3.0")
 local QL = LibStub("AceAddon-3.0"):GetAddon("QuestLink")
 
-local linkButtonLoaded = false;
+local linkButtonLoaded = false
 
 QuestLogDetailFrame:HookScript("OnShow", function(self)
     if linkButtonLoaded then return end
 
-    QuestLogDetailFrame.LinkBtn = CreateFrame("Button", nil, QuestLogDetailFrame, "OptionsButtonTemplate");
-    QuestLogDetailFrame.LinkBtn:SetPoint("TOP", -35, -45);
-    QuestLogDetailFrame.LinkBtn:SetSize(100,20);
-    QuestLogDetailFrame.LinkBtn:SetText("Open In Db");
-    QuestLogDetailFrame.LinkBtn:Show();
+    QuestLogDetailFrame.LinkBtn = CreateFrame("Button", "QuestLogDetailFrameDB", QuestLogDetailFrame, "AscenDBbuttonTemplate" )
+    QuestLogDetailFrame.LinkBtn:SetPoint("TOP", -35, -45)
+    QuestLogDetailFrame.LinkBtn.tooltipTitle = VIEW_ASCENSION_DB
+    QuestLogDetailFrame.LinkBtn.tooltipText = "Opens your browser to this quest on https://db.ascension.gg/"
+    QuestLogDetailFrame.LinkBtn:Show()
     QuestLogDetailFrame.LinkBtn:SetScript("OnClick", function(self,button)
         if IsControlKeyDown() then
             Internal_CopyToClipboard("https://db.ascension.gg/?quest="..select(9,GetQuestLogTitle(GetQuestLogSelection())))
         else
             OpenAscensionDBURL("?quest="..select(9,GetQuestLogTitle(GetQuestLogSelection())))
         end
-    end);
-    linkButtonLoaded = true;
-end);
+    end)
+    linkButtonLoaded = true
+end)
 
-local linkButtonLoadedQframe = false;
+local linkButtonLoadedQframe = false
 
 QuestLogFrame:HookScript("OnShow", function(self)
     if linkButtonLoadedQframe then return end
 
-    QuestLogFrame.LinkBtn = CreateFrame("Button", nil, QuestLogFrame, "OptionsButtonTemplate");
-    QuestLogFrame.LinkBtn:SetPoint("BOTTOMRIGHT", -90, 14);
-    QuestLogFrame.LinkBtn:SetSize(100,20);
-    QuestLogFrame.LinkBtn:SetText("Open In Db");
-    QuestLogFrame.LinkBtn:Show();
+    QuestLogFrame.LinkBtn = CreateFrame("Button", "QuestLogFrameDB", QuestLogFrame, "AscenDBbuttonTemplate")
+    QuestLogFrame.LinkBtn:SetPoint("BOTTOMRIGHT", -90, 14)
+    QuestLogFrame.LinkBtn:SetText("Open In Db")
+    QuestLogFrame.LinkBtn:Show()
+    QuestLogFrame.LinkBtn.tooltipTitle = VIEW_ASCENSION_DB
+    QuestLogFrame.LinkBtn.tooltipText = "Opens your browser to this quest on https://db.ascension.gg/"
     QuestLogFrame.LinkBtn:SetScript("OnClick", function(self,button)
         if IsControlKeyDown() then
             Internal_CopyToClipboard("https://db.ascension.gg/?quest="..select(9,GetQuestLogTitle(GetQuestLogSelection())))
         else
             OpenAscensionDBURL("?quest="..select(9,GetQuestLogTitle(GetQuestLogSelection())))
         end
-    end);
-    linkButtonLoadedQframe = true;
-end);
+    end)
+    linkButtonLoadedQframe = true
+end)
 
-local linkButtonLoadedMframe = false;
+local linkButtonLoadedMframe = false
 
 WorldMapQuestScrollFrame:HookScript("OnShow", function(self)
     if linkButtonLoadedMframe then return end
 
-    WorldMapQuestScrollFrame.LinkBtn = CreateFrame("Button", nil, WorldMapQuestScrollFrame, "OptionsButtonTemplate");
-    WorldMapQuestScrollFrame.LinkBtn:SetPoint("RIGHT", WorldMapZoomOutButton,110, 0);
-    WorldMapQuestScrollFrame.LinkBtn:SetSize(110,21);
-    WorldMapQuestScrollFrame.LinkBtn:SetText("Open In Db");
-    WorldMapQuestScrollFrame.LinkBtn:Show();
+    WorldMapQuestScrollFrame.LinkBtn = CreateFrame("Button", "WorldMapQuestScrollFrameDB", WorldMapQuestScrollFrame, "AscenDBbuttonTemplate" )
+    WorldMapQuestScrollFrame.LinkBtn:SetPoint("RIGHT", WorldMapZoomOutButton,110, 0)
+    WorldMapQuestScrollFrame.LinkBtn:Show()
+    WorldMapQuestScrollFrame.LinkBtn.tooltipTitle = VIEW_ASCENSION_DB
+    WorldMapQuestScrollFrame.LinkBtn.tooltipText = "Opens your browser to this quest on https://db.ascension.gg/"
     WorldMapQuestScrollFrame.LinkBtn:SetScript("OnClick", function(self,button)
         if IsControlKeyDown() then
             Internal_CopyToClipboard("https://db.ascension.gg/?quest="..select(9,GetQuestLogTitle(GetQuestLogSelection())))
         else
             OpenAscensionDBURL("?quest="..select(9,GetQuestLogTitle(GetQuestLogSelection())))
         end
-    end);
-    linkButtonLoadedMframe = true;
-end);
+    end)
+    linkButtonLoadedMframe = true
+end)
 
 --[[
 SlashCommand(msg):
@@ -79,9 +80,9 @@ end
 
 function QL:OnEnable()
     --Enable the use of /db to open the loot browser
-    SLASH_QUESTLINK1 = "/db";
+    SLASH_QUESTLINK1 = "/db"
     SlashCmdList["QUESTLINK"] = function(msg)
-        SlashCommand(msg);
+        SlashCommand(msg)
     end
 end
 
